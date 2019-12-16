ee8862bbc66b698b90770c93857f44bce3935a79
jack13163 <18163132129@163.com>
2019-12-17 00:07:33 +0800

    modefy

---
+++ src/org/rl4j/CustomEnv.java
@@ -0,0 +1,43 @@
+package org.rl4j;
+
+import org.deeplearning4j.gym.StepReply;
+import org.deeplearning4j.rl4j.mdp.MDP;
+import org.deeplearning4j.rl4j.space.ActionSpace;
+import org.deeplearning4j.rl4j.space.ObservationSpace;
+
+public class CustomEnv<O, A, AS extends ActionSpace<A>> implements MDP<O,A,AS> {
+    @Override
+    public ObservationSpace<O> getObservationSpace() {
+        return null;
+    }
+
+    @Override
+    public AS getActionSpace() {
+        return null;
+    }
+
+    @Override
+    public O reset() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public StepReply<O> step(A a) {
+        return null;
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public MDP<O, A, AS> newInstance() {
+        return null;
+    }
+}
---
+++ src/org/rl4j/fly/Enemy.java
@@ -0,0 +1,9 @@
+package org.rl4j.fly;
+
+/**
+ * 敌人，可以有分数
+ */
+public interface Enemy {
+	/** 敌人的分数  */
+	int getScore();
+}
