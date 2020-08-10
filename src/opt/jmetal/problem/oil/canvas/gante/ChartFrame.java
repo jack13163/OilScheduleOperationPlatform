5b29af13fc9e0b271e7a4b97f9e3868d18bcbad0
jack13163 <18163132129@163.com>
2020-08-10 15:57:37 +0800

    modefy

--- src/opt/jmetal/problem/oil/canvas/gante/ChartFrame.java
+++ src/opt/jmetal/problem/oil/canvas/gante/ChartFrame.java
@@ -1,6 +1,8 @@
 package opt.jmetal.problem.oil.canvas.gante;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class ChartFrame {
     // 窗口大小
@@ -13,6 +15,7 @@ public class ChartFrame {
     public ChartFrame() {
         frame = new JFrame("原油调度甘特图");
         canvas = new CanvasGante();
+
         // 将窗口定位到屏幕中间
         int screenWidth = (int) java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
         int screenHeight = (int) java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -20,6 +23,14 @@ public class ChartFrame {
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
         frame.add(canvas);
+
+        // 监听到JFrame窗口大小改变事件时，自动重绘canvas
+        frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                canvas.repaint();
+            }
+        });
     }
 
     public void updateCanvas(double[][] data) {
