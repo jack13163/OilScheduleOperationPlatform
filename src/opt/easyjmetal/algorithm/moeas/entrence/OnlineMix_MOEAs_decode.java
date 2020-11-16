a263acafc474fb2f9072e31e59eb093c87a34a0d
jack13163 <18163132129@163.com>
2020-11-17 00:58:35 +0800

    modefy

--- src/opt/easyjmetal/algorithm/moeas/entrence/OnlineMix_MOEAs_decode.java
+++ src/opt/easyjmetal/algorithm/moeas/entrence/OnlineMix_MOEAs_decode.java
@@ -8,8 +8,10 @@ import opt.easyjmetal.problem.onlinemix.OnlineMixOIL;
 import opt.easyjmetal.util.JMException;
 import opt.easyjmetal.util.sqlite.SqlUtils;
 
+import java.util.List;
+
 /**
- * 瑙ｇ爜
+ * 解码
  */
 public class OnlineMix_MOEAs_decode {
 
@@ -19,23 +21,41 @@ public class OnlineMix_MOEAs_decode {
         String tableName = "NSGAII_1";
 
         SolutionSet solutionSet = SqlUtils.SelectData(dbName, tableName);
+        int count = 0;
         for (int i = 0; i < solutionSet.size(); i++) {
-            evaluate(solutionSet.get(i));
+            boolean flag = evaluate(solutionSet.get(i));
+            if (flag) {
+                count++;
+            }
         }
+
+        // 输出结果一致比例
+        System.out.println("共有" + solutionSet.size() + "个非支配解，其中" + count + "个解和预期结果一致");
     }
 
     /**
-     * 璇勪环瑙ｇ殑閫傚簲搴︼紝骞惰繘琛屽彲瑙嗗寲
+     * 评价解的适应度，并进行可视化
+     *
      * @param solution
      * @throws JMException
      */
-    public static void evaluate(Solution solution) throws JMException {
+    public static boolean evaluate(Solution solution) throws JMException {
 
         Variable[] decisionVariables = solution.getDecisionVariables();
         double[][] x = new double[1][decisionVariables.length];
         for (int i = 0; i < x.length; i++) {
             x[0][i] = decisionVariables[i].getValue();
         }
-        Oilschdule.fat(x, true);
+
+        List<List<Double>> eff = Oilschdule.fat(x, false);
+
+        // 对比目标值是否前后一致
+        if (solution.getObjective(0) == eff.get(0).get(x.length + 0)
+                && solution.getObjective(1) == eff.get(0).get(x.length + 1)
+                && solution.getObjective(2) == eff.get(0).get(x.length + 2)
+                && solution.getObjective(3) == eff.get(0).get(x.length + 3)) {
+            return true;
+        }
+        return false;
     }
 }
